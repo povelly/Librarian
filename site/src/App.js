cca05bd69dde958d3debeb0b6ddfd1293f21c28e
saya-nel <pablobellotw@gmail.com>
2021-01-17 12:17:18 +0100

    Formulaire recherche + zone resultats

--- site/src/App.js
+++ site/src/App.js
@@ -1,7 +1,11 @@
 import Search from "./components/Search";
 import NavigationBar from "./components/NavigationBar";
 
+// css pour react-bootstrap
 import 'bootstrap/dist/css/bootstrap.min.css';
+// css global de la webapp
+import "./style/global.css";
+
 
 function App() {
   return (
--- site/src/components/Search.js
+++ site/src/components/Search.js
@@ -1,10 +1,71 @@
 import React, { Component } from "react";
+import { Button, Form, Alert } from "react-bootstrap";
+
+import "../style/search.css"
 
 export default class Search extends Component {
 
+    constructor(props) {
+        super(props)
+        this.state = {
+            searchArea: "",
+            searchType: "keyword",
+            hasSubmit: false,
+            results: []
+        }
+    }
+
+    handleChange = event => {
+        event.preventDefault();
+        this.setState({
+            [event.target.name]: event.target.value
+        });
+    }
+
+    handleSubmit = () => {
+        this.setState({
+            hasSubmit: true,
+        });
+    }
+
     render() {
         return (
-            <div>Bonjour</div>
+            <div className="componentContainer">
+                <div className="searchContainer">
+                    <Form id="searchForm">
+                        <Form.Control
+                            type="textarea"
+                            id="searchArea"
+                            name="searchArea"
+                            value={this.state.searchArea}
+                            placeholder="recherche"
+                            onChange={this.handleChange}
+                            maxLength="100"
+                        />
+                        <Form.Control as="select" name="searchType" onChange={this.handleChange}>
+                            <option value="keyword">Par mot-clé</option>
+                            <option value="regex">Par RegEx</option>    
+                        </Form.Control> 
+    
+                        <Button variant="dark" onClick={this.handleSubmit}>
+                            Rechercher
+                        </Button>
+                    </Form>
+                    <div id="results">
+                    {
+                        this.state.hasSubmit && this.state.results.length > 0?
+                        (
+                            <p>cc</p>
+                        ):
+                        (
+                            <Alert id="noResults" variant="info">
+                                <p>Aucun résultats trouvés, faites une recherche ou changez vos critères de recherche.</p>
+                            </Alert>
+                        )
+                    }
+                    </div>
+                </div>
+            </div>
         );
     }
 }
