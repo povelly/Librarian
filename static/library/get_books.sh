dc1e045fd7e829625052b31131d7d68761b9d8b4
saya-nel <pablobellotw@gmail.com>
2021-01-24 14:04:27 +0100

    Correction bug script get_book + nouvelle base livres

--- static/library/get_books.sh
+++ static/library/get_books.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
-CURL_BASE_COMMAND="curl http://www.gutenberg.org/files/"
+book_id=100
+count=0
+while [ "$count" -lt 2000 ]
+do
+    URL="http://www.gutenberg.org/files/$book_id/$book_id-0.txt"
+    CURL=$(curl $URL -o "$book_id.txt" --fail  2>&1)
 
-for i in {5000..7000}
-do 
-    CURL_FULL_COMMAND="$CURL_BASE_COMMAND$i/$i-0.txt -o \"$i.txt\""
-    eval $CURL_FULL_COMMAND
+    if [ $? -ne 0 ] 
+    then
+        echo "$book_id not found"
+    else
+        count=$((count + 1))
+    fi
+    book_id=$((book_id + 1))
 done
